62d9e21367abf0de0b357c540b7fef7d0b714a8f
wouter_kiesewetter <wouter@rometron.nl>
2023-11-24 14:37:40 +0100

    Start commit

---
+++ Day1/Day1.py
@@ -0,0 +1,41 @@
+import os
+
+day_number = 1
+
+def Part1(input):
+    result = 0
+
+    for line in input.splitlines():
+        print(line)
+    
+    print(f'Part 1: {result}')
+
+def Part2(input):
+    result = 0
+
+    for line in input.splitlines():
+        print(line)
+    
+    print(f'Part 2: {result}')
+
+def ReadInput():
+    filePath = ''
+    if os.path.isfile('input.txt'):
+        filePath = 'input.txt'
+    elif os.path.isfile(f'Day{day_number}/input.txt'):
+        filePath = f'Day{day_number}/input.txt'
+    
+    file = open(filePath, 'r')
+    data = file.read()
+    file.close()
+
+    return data
+
+def Run():
+    print(f'Day {day_number}')
+    input = ReadInput()
+    Part1(input)
+    Part2(input)
+
+if __name__ == "__main__":
+    Run()
---
+++ RunEveryDay.py
@@ -0,0 +1,8 @@
+from Day1.Day1 import Run as RunDay1
+
+if __name__ == "__main__":
+    days = [RunDay1]
+    
+    for d in days:
+
+        d()
